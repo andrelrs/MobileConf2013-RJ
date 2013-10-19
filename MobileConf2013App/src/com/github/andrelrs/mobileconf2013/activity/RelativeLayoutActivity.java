06af8a1d6907a7acc9e88a0f7f43b5cc1ebac7ec
André Silva <macolinkalkin@gmail.com>
2013-10-19 02:21:59 -0300

    Desafio 3

---
+++ MobileConf2013App/src/com/github/andrelrs/mobileconf2013/activity/RelativeLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.github.andrelrs.mobileconf2013.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.github.andrelrs.mobileconf2013.R;
+
+public class RelativeLayoutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_relative_layout);
+	}
+
+}
