e390e88afd484a797e5736f2dff6f7aac3a99156
André Silva <macolinkalkin@gmail.com>
2013-10-19 01:46:15 -0300

    Desafio 3

---
+++ MobileConf2013App/src/com/github/andrelrs/mobileconf2013/activity/GridLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.github.andrelrs.mobileconf2013.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.github.andrelrs.mobileconf2013.R;
+
+public class GridLayoutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_grid_layout);
+	}
+
+}
